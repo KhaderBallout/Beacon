b5dddf0767de9a0dd75c1eccb236c93b53b454c5
Khader Ballout <khaderj@hotmail.com>
2020-08-10 23:26:07 +0300

    Adding Link to WelcomePage

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import React from 'react';
 // import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import WelcomePage from './components/WelcomePage/WelcomePage';
 
 const App = () => {
   return (
     <Router>
-      {/* <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} /> */}
+      { <Route path="/" exact component={WelcomePage} />
+      /*<Route path="/chat" component={Chat} /> */}
     </Router>
   );
 }
--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const WelcomePage = () =>{
@@ -9,9 +9,9 @@ const WelcomePage = () =>{
         <div className="OuterContainer">
             <div className ="InnerContainer">
                <img className="Logo"></img>
-               <div><input className="input" placeholder="Full name" type="text" onChange={} /></div>
-               <div><input className="input" placeholder="Country name" type="text" onChange={} /></div>
-               <Link>
+               <div><input className="input" placeholder="Full name" type="text" onChange={(event) => setName(event.target.value)} /></div>
+               <div><input className="input" placeholder="Country name" type="text" onChange={(event) => setCountry(event.target.value)} /></div>
+               <Link to={`/chat?name${name}&country${country}`}>
                <button className ="button" type="submit">Enter Global Chat </button>
                </Link>
             </div>
