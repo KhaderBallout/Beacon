ce2f419d05fed9a91fe888961b1f253b2b286530
Khader Ballout <khaderj@hotmail.com>
2020-08-10 23:33:31 +0300

    Adding BackGround Image

--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
-
+import './WelcomePage.css';
 const WelcomePage = () =>{
     const[name, setName] = useState('');
     const [country,setCountry]= useState('');
@@ -11,7 +11,7 @@ const WelcomePage = () =>{
                <img className="Logo"></img>
                <div><input className="input" placeholder="Full name" type="text" onChange={(event) => setName(event.target.value)} /></div>
                <div><input className="input" placeholder="Country name" type="text" onChange={(event) => setCountry(event.target.value)} /></div>
-               <Link to={`/chat?name${name}&country${country}`}>
+               <Link onClick={event => (!name || !country) ? event.preventDefault(): null} to={`/chat?name${name}&country${country}`}>
                <button className ="button" type="submit">Enter Global Chat </button>
                </Link>
             </div>
