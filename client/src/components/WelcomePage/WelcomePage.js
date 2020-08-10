5b74cc5cbda28704aa849a2942126e8ece4fffc8
Khader Ballout <khaderj@hotmail.com>
2020-08-10 23:17:29 +0300

    Adding classname to welcome page

--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const WelcomePage = () =>{
     const[name, setName] = useState('');
     const [country,setCountry]= useState('');
 
-    
+    return (
+        <div className="OuterContainer">
+            <div className ="InnerContainer">
+               <img className="Logo"></img>
+               <div><input className="input" placeholder="Full name" type="text" onChange={} /></div>
+               <div><input className="input" placeholder="Country name" type="text" onChange={} /></div>
+               <Link>
+               <button className ="button" type="submit">Enter Global Chat </button>
+               </Link>
+            </div>
+        </div>
+    )
+
 }
+
+export default WelcomePage;
