71a5f8cdac93183fc7764f5f6c59f0d754e1ff94
Gharam <gharamissa@gmail.com>
2020-08-11 20:25:05 +0300

    first editions

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import Chat from './components/chat/Chat';
 // import Join from './components/Join/Join';
+import WelcomePage from './components/WelcomePage/WelcomePage'
 
-=======
-// import Chat from './components/Chat/Chat';
-import WelcomePage from './components/WelcomePage/WelcomePage';
->>>>>>> ae34831f469515ab8ed2d4b5de2fc069bb9e307c
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-<<<<<<< HEAD
-     {/* <Route path="/" exact component={Join} /> */}
-     <Route path="/chat" component={Chat} /> 
-=======
       <Route path="/" exact component={WelcomePage} />
-      {/* <Route path="/chat" component={Chat} /> */}
->>>>>>> ae34831f469515ab8ed2d4b5de2fc069bb9e307c
+      <Route path="/chat" component={Chat} />
     </Router>
   );
 }
