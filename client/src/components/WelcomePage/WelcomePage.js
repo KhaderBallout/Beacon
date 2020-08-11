1d74c1c48bfe52b9258693ef3857d04a705e96bf
JannatN <jannatfaisalnatsheh@gmail.com>
2020-08-11 19:09:00 +0300

    welcome page style done

--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,40 +1,48 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './WelcomePage.css';
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
+
 
 const WelcomePage = () => {
-    const [name, setName] = useState("");
-    const [country, setCountry] = useState("");
-
+    const [name, setName] = useState('');
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
 
     return (
-        <div className="card">
-            <div className="card-top"><img src={require('./Beacon.png')} height="200px" width="400px"></img></div>
-            <div className="inputGroup">
-                <label className="labels"> Name: </label> &nbsp;&nbsp;&nbsp;
-                <input className="inputFeilds"
-                    type="text"
-                    name="name"
-                    placeholder="Full name"
-                    value={name}
-                    onChange={e => {
-                        setName(e.target.value);
-                    }}
-                ></input><br></br>
-                <label className="labels"> Country: </label>
-                <input className="inputFeilds"
-                    type="text"
-                    country="country"
-                    placeholder="Country"
-                    value={country}
-                    onChange={e => {
-                        setCountry(e.target.value);
-                    }}
-                ></input>
+        <div className="OuterContainer">
+            <div className="InnerContainer">
+                <div className="Logo" ><img src={require('./Beacon.png')} height="200px" width="400px"></img></div>
+
+                <div className="inputGroup">
+                    <label className="label" >Name:</label>
+                    <input className="inputFeilds" placeholder="Full name" type="text"
+                        onChange={(event) => setName(event.target.value)} />
+                    <label className="label" >Country:</label>
+
+                    <CountryDropdown
+                        className="country"
+                        value={country}
+                        labelType="short"
+                        valueType="short"
+                        onChange={(e) => setCountry(e)} />
+                    <label >City:</label>
+
+                    <RegionDropdown
+                        className="country"
+                        country={country}
+                        value={city}
+                        countryValueType="short"
+                        placeholder="Select Country"
+                        onChange={(e) => setCity(e)} />
+                </div>
+                <Link onClick={event => (!name || !(country && city)) ? event.preventDefault() : null} to={`/chat?name=${name}&country=${country}&key=${city}`}>
+                    <div className="button"> <button id="button" type="submit">Enter Global Chat </button></div>
+                </Link>
             </div>
-            <div className="btnDiv"> <button className="button" type="submit">Enter Global Chat </button></div>
         </div>
-    );
-};
+    )
+
+}
 
 export default WelcomePage;
