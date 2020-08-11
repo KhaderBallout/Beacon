b58e73dc9c2ab1c086265c5dc0cd6c67ada7b440
Khader Ballout <khaderj@hotmail.com>
2020-08-11 12:18:25 +0300

    Editing style

--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -8,11 +8,11 @@ const WelcomePage = () =>{
     return (
         <div className="OuterContainer">
             <div className ="InnerContainer">
-               <img className="Logo"></img>
-               <div><input className="input" placeholder="Full name" type="text" onChange={(event) => setName(event.target.value)} /></div>
-               <div><input className="input" placeholder="Country name" type="text" onChange={(event) => setCountry(event.target.value)} /></div>
+               <div className="Logo" ><img src = "Beacon.png" height="200px" width="300px"></img></div>
+               <div className="input">Name:&nbsp;&nbsp;&nbsp;<input  placeholder="Full name" type="text" onChange={(event) => setName(event.target.value)} /></div>
+               <div className="input">Country:<input placeholder="Country name" type="text" onChange={(event) => setCountry(event.target.value)} /></div>
                <Link onClick={event => (!name || !country) ? event.preventDefault(): null} to={`/chat?name${name}&country${country}`}>
-               <button className ="button" type="submit">Enter Global Chat </button>
+               <div className ="button"> <button id="button" type="submit">Enter Global Chat </button></div>
                </Link>
             </div>
         </div>
