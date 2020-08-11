e478eabafd07ed199c9a6d31f31e29bf47330780
Ramiz Rizqallah <ramiz_rizqallah@hotmail.com>
2020-08-11 17:23:09 +0300

    Server handles connections and disconnections

--- server/controllers.js
+++ server/controllers.js
@@ -1,47 +1,48 @@
 const axios = require('axios');
-const users = []; //total number of users is the length of the array
-const countries = []; //total number of countries is the length of the array
+const users = 0; //total number of users is the length of the array
+const countries = {}; //total number of countries is the length of values 
 const messages = []; //total number of messages is the length of the array
 
-const addCountry = ({city,country}) => {
+
+const addCountry = ({ city, countryCode }) => {
     //convert city or country to Country code
     //https://restcountries.eu/rest/v2/name/{NAME} This is the Api Example to get country code
     var flag;
-    axios.get(`https://restcountries.eu/rest/v2/alpha/${country}`)
-    .then(resp => {
+    axios.get(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
+        .then(resp => {
 
-    console.log(resp.data);
-    flag = resp.data.flag
-    });
+            console.log(resp.data);
+            flag = resp.data.flag
+        });
 
-    //check for existance
-    const doesExist = locations.find((cntry) => cntry === country)
-    if(doesExist){
-        //add lat lon on map and return flag
-        return flag;
+    if (countries[countryCode]) {
+        countries[countryCode] += 1;
     }
-    else{
-        locations.push(country)
-        //add lat lon on map and return flag
-        return flag;
+    else {
+        countries[countryCode] = 1;
     }
+    console.log(countries)
+
+    //Send City Lat Lon to MAP HERE
+
+
+    //return flag and number of countries
+    return {flag, numOfCountries: Object.keys(countries).length};
+
 }
 
-const addMessage = ({sender,country,text}) =>{
-    messages.push({sender,country,text})
-    return {sender,country,text}
+const addMessage = ({ sender, country, text }) => {
+    messages.push({ sender, country, text })
+    return { sender, country, text }
 }
 
-const addUser = ({ id, name}) => {
- 
-     const user = {id,name,country}
-     users.push(user)
-     return {user}
- 
+const addUser = () =>{
+    users++;
+    console.log("Number of current users",users)
 }
 
-const getUser = (id) => {
-    return users.find((user)=> user.id ===id)
+const getMessages =()=>{
+    return messages;
 }
 
-module.exports= {addUser,getUser,addMessage,addLocation}
+module.exports = { getMessages, addUser, addMessage, addCountry }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const {addUser,getUser,addMessage,addLocation} = require('./controllers')
+const { getMessages, addUser, addMessage, addCountry } = require('./controllers')
 
 //Using Router for handling Get request on Server Startup 
 const router = require('./router');
@@ -15,17 +15,22 @@ const io = socket(server)
 
 //Defining an env.PORT or a static 5000 Port for the server to listen on
 const PORT = process.env.PORT || 5000;
-server.listen( PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
 //Handling All Socket Emits, Broadcasts, and calls.
-io.on('connection', (socket)=>{
-    socket.on('join',({name,location})=>{
-        const {error,user} =addUser({id:socket.id,name,location})
-
-        console.log(name," from ",country," has joined!")
+io.on('connection', (socket) => {
+    socket.on('join', ({ name, location }) => {
+        //addUser();
+        //addCountry(location)
+        //socket.broadcast('messages', getMessages());
+        console.log(name, " from ", location.country, " has joined!")
     })
 
-    io.on('disconnect', ()=>{
+    socket.on('send', ({ sender, country, text }) => {
+        addMessage({ sender, country, text })
+    })
+
+    socket.on('disconnect', () => {
         console.log("A connection has been dropped")
     })
 })
