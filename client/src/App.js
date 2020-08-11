e989d50d5b77644f1020407cdcb4c07fe9b54d65
Gharam <gharamissa@gmail.com>
2020-08-11 20:08:07 +0300

    second page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import Chat from './components/Chat/Chat';
+import Chat from './components/chat/Chat';
 // import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,8 +8,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      {/* <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} /> */}
+     {/* <Route path="/" exact component={Join} /> */}
+     <Route path="/chat" component={Chat} /> 
     </Router>
   );
 }
