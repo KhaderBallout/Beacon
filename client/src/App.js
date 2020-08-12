743a67cf8537ef6bc1ba2005c2c097dbdac12fca
Khader Ballout <khaderj@hotmail.com>
2020-08-12 11:27:06 +0300

    Final design for welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Chat from './components/chat/Chat';
 // import Join from './components/Join/Join';
-import WelcomePage from './components/WelcomePage/WelcomePage'
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import WelcomePage from './components/WelcomePage/WelcomePage';
@@ -10,13 +9,8 @@ import WelcomePage from './components/WelcomePage/WelcomePage';
 const App = () => {
   return (
     <Router>
-<<<<<<< HEAD
-      { <Route path="/" exact component={WelcomePage} />
-      /*<Route path="/chat" component={Chat} /> */}
-=======
       <Route path="/" exact component={WelcomePage} />
       <Route path="/chat" component={Chat} />
->>>>>>> b653948acdfc634e71949906374cf5d27d5db1e9
     </Router>
   );
 }
