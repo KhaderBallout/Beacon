3e71d05a64b089693b77800753b7d33ce85f5c84
Gharam <gharamissa@gmail.com>
2020-08-12 16:30:13 +0300

    chat pannel editions

---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Message.css'
+import ScrollToBottom from 'react-scroll-to-bottom'
+const Messages = () => (
+
+  
+ <ScrollToBottom>
+     
+ </ScrollToBottom>
+)
+
+
+
+export default Messages
