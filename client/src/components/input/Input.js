46cf36d7c6d869a79cf8ba1e66298b4abd8b58a9
Gharam <gharamissa@gmail.com>
2020-08-14 01:40:12 +0300

    emokis picker

--- client/src/components/input/Input.js
+++ client/src/components/input/Input.js
@@ -1,23 +1,47 @@
 
-import React from 'react'
-
+import React, { useState } from 'react';
 import './Input.css'
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
-const Input = ({ message, setMessage, send }) => (
-  <div className="form">
+const Input = ({ message, setMessage, send }) => {
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+  const [display, setDisplay] = useState(false);
 
-    <input
-      className="input-box"
-      type="text"
-      value={message}
-      placeholder="Send a supportive message :)"
-      onChange={(e) => setMessage(e.target.value)}
-      onKeyPress={(e => e.key === 'Enter' ? send(e) : null)}
-    />
-    <button className="sendButton" onClick={(e) => send(e)}></button>
+  let emojiPicker;
+  if (display) {
+    emojiPicker = (
+      <Picker 
+        title="Pick your emoji…"
+        emoji="point_up"
+        onSelect={emoji => setMessage(message + emoji.native)}
+      />
+    );
+  }
 
-  </div>
-)
+  function emojiEvent(e) {
+    e.preventDefault();
+    setDisplay(!display)
+  }
+
+
+
+  return (
+    <div className="form">
+      <input
+        className="input-box"
+        type="text"
+        value={message}
+        placeholder="Send a supportive message :)"
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e => e.key === 'Enter' ? send(e) : null)}
+      />
+      {emojiPicker}
+      <button className="sendButton" onClick={(e) => { send(e); emojiEvent(e) }}></button>
+      <button className="emojiBtn" onClick={emojiEvent} />
+    </div>
+  );
+}
 
 
 
