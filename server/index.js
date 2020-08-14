33264f21d633635a8226d810a82f203afa53b89e
Khader Ballout <khaderj@hotmail.com>
2020-08-14 14:51:26 +0300

    heruko

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const
+const cors = require('cors')
 const { getInfo, addUser, addMessage, addCountry} = require('./controllers')
 
 //Using Router for handling Get request on Server Startup 
 const router = require('./router');
 app.use(router);
-
+app.use(cors());
 //requiring all necessary libraries for Socket.io
 const socket = require('socket.io')
 const io = socket(server)
@@ -17,6 +17,7 @@ const io = socket(server)
 //Defining an env.PORT or a static 5000 Port for the server to listen on
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
 
 //Handling All Socket Emits, Broadcasts, and calls.
 io.on('connection', (socket) => { //client socket
