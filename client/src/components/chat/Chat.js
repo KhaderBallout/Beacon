3c3213fef98636cf50c1b9cb19cda68d85852c03
Khader Ballout <khaderj@hotmail.com>
2020-08-14 15:11:43 +0300

    heroku url

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -8,7 +8,7 @@ import Map from '../map/Map'
 import Messages from '../Messages/Messages';
 
 const Chat = ({ location }) => {
-    const ENDPOINT = "http://localhost:5000"
+    const ENDPOINT = "https://beacon-by-root.herokuapp.com/"
     const [name, setName] = useState('');
     const [country, setCountry] = useState('');
     const [message, setMessage] = useState(''); //message from input
